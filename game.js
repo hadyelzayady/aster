513468bac8aacc8a845138e6a51036f74f1d9013
hadyelzayady <hadyelzayady@yahoo.com>
2017-01-18 02:01:13 +0200

    add color to coins

--- game.js
+++ game.js
@@ -722,7 +722,8 @@ Coin = function () {
               0,  5,
               5,  0,
               0, -5]);
-
+  this.color    = 'purple';
+  this.solid    = true;
   this.collidesWith = ["ship"];
 
   this.newPosition = function () {
